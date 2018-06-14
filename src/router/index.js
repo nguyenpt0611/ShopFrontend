7092b33a527f760a167899815f5291fece78cd58
ngueynpham <nguyen.pham@tripath.vn>
2018-06-15 00:17:45 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const LoginUser = () => import('@/components/LoginUser')
-const Package = () => import('@/components/Package')
-const PackageRegister = () => import('@/components/PackageRegister')
-const PackageDetail = () => import('@/components/PackageDetail')
 
 Vue.use(Router)
 
@@ -18,29 +14,29 @@ export default new Router({
     {
       path: '/package',
       name: 'Package',
-      component: Package
+      component: require('@/components/LoginUser')
     },
     {
       path: '/login',
       name: 'LoginUser',
-      component: LoginUser
+      component: require('@/components/LoginUser')
     },
     {
       path: '/package/register/:name?',
       name: 'PackageRegister',
-      component: PackageRegister,
+      component: require('@/components/PackageRegister'),
     },
     {
       path: '/package/detail/:name?',
       name: 'PackageDetail',
-      component: PackageDetail,
+      component: require('@/components/PackageDetail'),
     }
   ],
   scrollBehavior (to, from, savedPosition) {
     if(savedPosition){
       return savedPosition
     }
-    return { x: 0, y: 0 }
+    return { y: 0 }
   }
 
 })
